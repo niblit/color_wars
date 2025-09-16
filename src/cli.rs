36a9d3fb9c239f50d3ab4053f4d382308eab8bf3
niblit <git@niblit.dev>
2025-09-16 16:45:52 -0600

    Improve board formatting for printing

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,7 @@
 //! This module is responsible for two main tasks:
 //! 1. Printing a colorized, human-readable representation of the game board.
 //! 2. Prompting the user for input and validating their moves.
-use crate::{BOARD_ROW_SIZE, BOARD_COLUMN_SIZE, board::Board, coordinates::Coordinates, player::Player, square::Square};
+use crate::{board::{ Board}, coordinates::Coordinates, player::Player, square::Square, BOARD_COLUMN_SIZE, BOARD_ROW_SIZE};
 use colored::{ColoredString, Colorize};
 use std::io::Write;
 
@@ -19,9 +19,9 @@ use std::io::Write;
 /// The `Coordinates` of the valid move selected by the user.
 pub fn input_coordinates(valid_moves: &[Coordinates]) -> Coordinates {
     loop {
-        // Prompt for and parse column and row numbers.
-        let column = input_usize("Column: ");
+        // Prompt for and parse row and column numbers.
         let row = input_usize("Row: ");
+        let column = input_usize("Column: ");
 
         // Validate that the coordinates are on the board.
         if (0..BOARD_ROW_SIZE).contains(&row) && (0..BOARD_COLUMN_SIZE).contains(&column) {
@@ -63,54 +63,83 @@ fn input_usize(message: &str) -> usize {
 ///
 /// It displays the grid, coordinate hints, and the current player's turn.
 pub fn print_board(board: &Board) {
-    let mut lower_coordinates_hint = String::from("   ");
-    for i in 0..BOARD_COLUMN_SIZE {
-        lower_coordinates_hint = format!("{lower_coordinates_hint} {} ", i);
-    }
-    let column_coordinates = format!("{lower_coordinates_hint}   ");
-
-    let column_coordinates_hint = column_coordinates.white().on_black();
-
-    // Top coordinate hints
-    println!("{column_coordinates_hint}");
-
-    // Print each row with its contents and side hints.
-    for (i, &row) in board.grid().iter().enumerate() {
-        format_row_to_print(i, row);
-    }
-
-    // Bottom coordinate hints
-    println!("{column_coordinates_hint}");
-
-    // Display the current player's turn.
-    println!(
-        "Turn: {}",
-        match board.turn() {
-            Player::Red => " ▪ ".black().on_red(),
-            Player::Blue => " ▪ ".black().on_blue(),
-        }
-    );
+    println!("{}", format_board(board));
 }
 
-// Formats and prints a single row of the board, including side coordinate hints.
-fn format_row_to_print(i: usize, row: [Square; BOARD_COLUMN_SIZE]) {
-    let row_coordinates_hint = format!(" {i} ").white().on_black();
-    print!("{row_coordinates_hint}");
-    for square in row {
-        print!("{}", format_square_to_print(square));
+fn format_board(board: &Board) -> ColoredString {
+    let mut formatted_board = format_column_coordinates_hint();
+    for row_index in 0..BOARD_ROW_SIZE {
+        let formatted_row = format_single_row(row_index, board.grid()[row_index]);
+        formatted_board = format!("{formatted_board}{formatted_row}\n").into();
+
     }
-    println!("{row_coordinates_hint}");
+    formatted_board = format!("{formatted_board}{}", format_column_coordinates_hint()).into();
+
+    let formatted_turn = format_turn_bar(board.turn());
+
+    formatted_board = format!("{formatted_board}{formatted_turn}").into();
+
+    formatted_board
+}
+
+fn format_column_coordinates_hint() -> ColoredString {
+    let empty_pad = String::from("   ").on_black();
+    let mut column_coordinates_hint: ColoredString = empty_pad.clone();
+
+    for column_index in 0..BOARD_COLUMN_SIZE {
+        let formatted_index = format!(" {} ", column_index).white().on_black();
+        column_coordinates_hint = format!("{column_coordinates_hint}{formatted_index}").into();
+    }
+
+    column_coordinates_hint = format!("{column_coordinates_hint}{empty_pad}\n").into();
+
+    column_coordinates_hint
+}
+
+fn format_turn_bar(player: Player) -> ColoredString {
+    let turn = format_turn(player);
+    let mut turn_bar = String::new().into();
+    for _ in 0..(BOARD_COLUMN_SIZE + 2) {
+        turn_bar = format!("{turn_bar}{}", turn.clone()).into();
+    }
+
+    turn_bar
+}
+
+fn format_turn(player: Player) -> ColoredString {
+    let turn = String::from(" ▪ ").black();
+    let turn = match player {
+        Player::Red => turn.on_red(),
+        Player::Blue => turn.on_blue(),
+    };
+
+    turn
+}
+
+// Formats a single row of the board, including side coordinate hints.
+fn format_single_row(i: usize, row: [Square; BOARD_COLUMN_SIZE]) -> ColoredString {
+    let mut formatted_row: ColoredString = String::new().into();
+
+    let row_coordinates_hint = format!(" {i} ").white().on_black();
+    formatted_row = format!("{formatted_row}{row_coordinates_hint}").into();
+
+    for square in row {
+        formatted_row = format!("{formatted_row}{}", format_single_square(square)).into();
+    }
+    formatted_row = format!("{formatted_row}{row_coordinates_hint}").into();
+
+    formatted_row
 }
 
 // Formats a single square into a colored string for display.
-fn format_square_to_print(square: Square) -> ColoredString {
+fn format_single_square(square: Square) -> ColoredString {
     if let Some(player) = square.owner() {
-        let value = format!(" {} ", square.value());
+        let value = format!(" {} ", square.value()).black();
         match player {
-            Player::Red => value.black().on_red(),
-            Player::Blue => value.black().on_blue(),
+            Player::Red => value.on_red(),
+            Player::Blue => value.on_blue(),
         }
     } else {
-        String::from(" ▪ ").on_black()
+        String::from(" ▪ ").white().on_black()
     }
 }
